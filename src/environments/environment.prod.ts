ebd32d16464c1cf333bb0ba9aee1e2a02676d724
Alan Chen <alanchen@microsoft.com>
2018-09-16 00:06:44 -0700

    Adding prod values to environments.prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
-  facebookAppId: "246535755994969"
+  facebookAppId: "246535755994969",
+  watchlistApiGetCardsUri: "https://mtgwatchlist-api.azurewebsites.net/api/watchlist",
+  watchlistApiAddCardUri: "https://mtgwatchlist-api.azurewebsites.net/api/watchlist/add",
+  scryfallApiFuzzyFetchUrl: "https://api.scryfall.com/cards/named?fuzzy="
 };
